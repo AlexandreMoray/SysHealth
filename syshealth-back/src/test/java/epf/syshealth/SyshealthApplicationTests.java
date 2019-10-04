000e957e255ec5d166d90399cf64bfe971a84686
Alexandre MORAY <alexandre.moray@epfedu.fr>
2019-10-04 12:12:24 +0200

    structure files added to dev

---
+++ syshealth-back/src/test/java/epf/syshealth/SyshealthApplicationTests.java
@@ -0,0 +1,13 @@
+package epf.syshealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyshealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
